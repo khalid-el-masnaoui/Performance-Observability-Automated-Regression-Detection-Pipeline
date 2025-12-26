db9a5d36248575a2aabb2b7dc589244f5efa2ed8
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-12-26 12:13:00 +0100

    k6 entrypoint: error_rate query

--- k6/entrypoint.sh
+++ k6/entrypoint.sh
@@ -72,3 +72,102 @@ for route in "${ROUTES[@]}"; do
   P99=$(curl -sG "$PROM_URL/api/v1/query" \
     --data-urlencode "query=$P99_QUERY" \
     | jq -r '.data.result[0].value[1]')
+
+  # ---------------------------------------------------
+  # AVG
+  # ---------------------------------------------------
+
+  AVG_QUERY="
+    rate(app_request_duration_seconds_sum{route=\"$route\"}[2m])
+    /
+    rate(app_request_duration_seconds_count{route=\"$route\"}[2m])
+  "
+
+  AVG=$(curl -sG "$PROM_URL/api/v1/query" \
+    --data-urlencode "query=$AVG_QUERY" \
+    | jq -r '.data.result[0].value[1]')
+
+
+  # ---------------------------------------------------
+  # ERROR RATE
+  # ---------------------------------------------------
+
+  ERROR_QUERY="
+    sum(rate(app_requests_total{route=\"$route\",status=~\"5..\"}[2m]))
+    /
+    sum(rate(app_requests_total{route=\"$route\"}[2m]))
+  "
+
+  ERROR_RATE=$(curl -sG "$PROM_URL/api/v1/query" \
+    --data-urlencode "query=$ERROR_QUERY" \
+    | jq -r '.data.result[0].value[1]')
+
+    if [[ "$ERROR_RATE" == "null" || "$ERROR_RATE" == "NaN" || -z "$ERROR_RATE" ]]; then
+        ERROR_RATE=0
+    fi
+
+  # ---------------------------------------------------
+  # MAX LATENCY
+  # ---------------------------------------------------
+
+  MAX_QUERY="
+    max_over_time(
+      app_request_duration_seconds_sum{route=\"$route\"}[5m]
+    )
+  "
+
+  MAX_LATENCY=$(curl -sG "$PROM_URL/api/v1/query" \
+    --data-urlencode "query=$MAX_QUERY" \
+    | jq -r '.data.result[0].value[1]')
+
+  # ---------------------------------------------------
+  # THROUGHPUT
+  # ---------------------------------------------------
+
+  THROUGHPUT_RPS_QUERY="
+    sum(rate(app_request_duration_seconds_count{route=\"$route\"}[1m]))
+  "
+
+  THROUGHPUT_RPS=$(curl -sG "$PROM_URL/api/v1/query" \
+    --data-urlencode "query=$THROUGHPUT_RPS_QUERY" \
+    | jq -r '.data.result[0].value[1]')
+
+  # ---------------------------------------------------
+  # VALIDATION
+  # ---------------------------------------------------
+
+  if [ "$P95" == "null" ] || [ -z "$P95" ]; then
+    echo "⚠️ No p95 data for $route"
+    continue
+  fi
+
+  # ---------------------------------------------------
+  # DEFAULTS
+  # ---------------------------------------------------
+
+  AVG=${AVG:-0}
+  P99=${P99:-0}
+  ERROR_RATE=${ERROR_RATE:-0}
+  MAX_LATENCY=${MAX_LATENCY:-0}
+  THROUGHPUT_RPS=${THROUGHPUT_RPS:-0}
+
+  # ---------------------------------------------------
+  # LOGGING
+  # ---------------------------------------------------
+
+  echo "Route        : $route"
+  echo "P95          : $P95"
+  echo "P99          : $P99"
+  # ---------------------------------------------------
+  # AVG
+  # ---------------------------------------------------
+
+  AVG_QUERY="
+    rate(app_request_duration_seconds_sum{route=\"$route\"}[2m])
+    /
+    rate(app_request_duration_seconds_count{route=\"$route\"}[2m])
+  "
+
+  AVG=$(curl -sG "$PROM_URL/api/v1/query" \
+    --data-urlencode "query=$AVG_QUERY" \
+    | jq -r '.data.result[0].value[1]')
