dffe771fd475dc0018d459ee4a7c03acb04241c9
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-12-30 12:13:00 +0100

    local testing: storing baseline --> avg & error_rate queries

--- testing/scripts/store-baseline.sh
+++ testing/scripts/store-baseline.sh
@@ -55,4 +55,37 @@ for route in "${ROUTES[@]}"; do
     --data-urlencode "query=$P99_QUERY" \
     | jq -r '.data.result[0].value[1]')
 
+  # ---------------------------------------------------
+  # AVG
+  # ---------------------------------------------------
+
+  AVG_QUERY="
+    rate(app_request_duration_seconds_sum{route=\"$route\"}[2m])
+    /
+    rate(app_request_duration_seconds_count{route=\"$route\"}[2m])
+  "
+
+  AVG=$(curl -sG "$PROM_URL/api/v1/query" \
+    --data-urlencode "query=$AVG_QUERY" \
+    | jq -r '.data.result[0].value[1]')
+
+
+  # ---------------------------------------------------
+  # ERROR RATE
+  # ---------------------------------------------------
+
+  ERROR_QUERY="
+    sum(rate(app_requests_total{route=\"$route\",status=~\"5..\"}[2m]))
+    /
+    sum(rate(app_requests_total{route=\"$route\"}[2m]))
+  "
+
+  ERROR_RATE=$(curl -sG "$PROM_URL/api/v1/query" \
+    --data-urlencode "query=$ERROR_QUERY" \
+    | jq -r '.data.result[0].value[1]')
+
+    if [[ "$ERROR_RATE" == "null" || "$ERROR_RATE" == "NaN" || -z "$ERROR_RATE" ]]; then
+        ERROR_RATE=0
+    fi
+
  
