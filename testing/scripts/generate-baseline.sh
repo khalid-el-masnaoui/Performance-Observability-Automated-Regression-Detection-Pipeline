bc446f273a6c6fd03d3ecc2a0e82f748c773cd34
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-12-30 12:13:00 +0100

    local testing: generate baseline completed

--- testing/scripts/generate-baseline.sh
+++ testing/scripts/generate-baseline.sh
@@ -4,10 +4,13 @@ NGINX_URL="$PROM_URL"
 REGRESSION_URL="http://localhost:8090"
 PROM_URL="http://localhost:9090"
 
-ROUTES=(
-  "/"
-  "/api/users"
-)
+IFS=',' read -ra ROUTES <<< "$1"
+
+
+# ROUTES=(
+#   "/"
+#   "/api/users"
+# )
 
 #BASELINE_ROUTES=$(shell curl -s $PROM_URL/routes)
 
