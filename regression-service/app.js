9b010dc7e9f3aaf353fdb339f392978832a19675
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-01-08 11:30:00 +0100

    regression service configs

--- regression-service/app.js
+++ regression-service/app.js
@@ -4,3 +4,11 @@ const Redis = require("ioredis");
 
 const app = express();
 app.use(express.json());
+
+// --------------------
+// Config
+// --------------------
+const REDIS_HOST = process.env.REDIS_HOST || "redis";
+const PROM_URL = process.env.PROM_URL || "http://prometheus:9090";
+const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK;
+const REPORT_URL = (process.env.REPORT_URL || "http://report-service:5000");
